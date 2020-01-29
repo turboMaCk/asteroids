bed59e3eee12667debd45a2d098290a92c0a45db
Marek Fajkus <marek.faj@gmail.com>
2020-01-29 14:55:00 +0100

    multiline instructions

--- src/menu.c
+++ src/menu.c
@@ -8,11 +8,13 @@
 // This sort of crates a cyclic dependecy
 #include "game.h"
 
+#define INST_LINES 2
+
 Menu* Menu_init(SDL_Renderer *ren, Game* game)
 {
   Menu* res = malloc(sizeof(Menu));
 
-  char *title_text, *instructions_text;
+  char *title_text, *instructions[2];
   char score_text[64];
 
   snprintf(score_text, 64, "Score: %d", game->score);
@@ -24,12 +26,14 @@ Menu* Menu_init(SDL_Renderer *ren, Game* game)
   }
   case GameEnded: {
     title_text = "Game Ended";
-    instructions_text = "Press ENTER or Start for new game; Press ESC or Back to exit.";
+    instructions[0] = "Press ENTER or Start for new game.";
+    instructions[1] = "Press ESC or Back to exit.";
   } break;
   case GameNotStarted:
   case GamePaused: {
     title_text = "Game Paused";
-    instructions_text = "Press ENTER or Start to continue; Press ESC or Back to exit.";
+    instructions[0] = "Press ENTER or Start to continue.";
+    instructions[1] = "Press ESC or Back to exit.";
   } break;
   }
 
@@ -56,11 +60,17 @@ Menu* Menu_init(SDL_Renderer *ren, Game* game)
 
   // Instructions
   {
-    SDL_Color color = {255,255,255,255};
     TTF_Font* font = TTF_OpenFont("fonts/Monda-Regular.ttf", 24);
-    SDL_Surface* surf = TTF_RenderText_Blended(font, instructions_text, color);
-    res->instructions = SDL_CreateTextureFromSurface(ren, surf);
-    SDL_FreeSurface(surf);
+
+    SDL_Texture **textures = malloc(sizeof(SDL_Texture*)*2);
+    res->instructions = textures;
+    for (int i = 0; i < INST_LINES; ++i) {
+      SDL_Surface* surf = TTF_RenderText_Blended(font, instructions[i], color);
+      *textures = SDL_CreateTextureFromSurface(ren, surf);
+      SDL_FreeSurface(surf);
+      ++textures;
+    }
+
     TTF_CloseFont(font);
   }
 
@@ -87,16 +97,24 @@ void Menu_render(Menu* menu, SDL_Renderer* ren, int win_width, int win_height)
 
   // Render Instructions
   {
-    int width, height;
-    SDL_QueryTexture(menu->instructions, NULL, NULL, &width, &height);
-    SDL_Rect dest = {win_width/2 - width/2, win_height/2 - height/2 + 110, width, height};
-    SDL_RenderCopy(ren, menu->instructions, NULL, &dest);
+    for (int i = 0; i < INST_LINES; ++i) {
+      int width, height;
+      SDL_QueryTexture(menu->instructions[i], NULL, NULL, &width, &height);
+      SDL_Rect dest = {win_width/2 - width/2, win_height/2 - height/2 + 110 + (40*i), width, height};
+      SDL_RenderCopy(ren, menu->instructions[i], NULL, &dest);
+    }
   }
 }
 
 void Menu_destroy(Menu* menu)
 {
   SDL_DestroyTexture(menu->title);
+  SDL_DestroyTexture(menu->score);
 
+  for (int i = 0; i < INST_LINES; ++i) {
+    SDL_DestroyTexture(menu->instructions[i]);
+  }
+
+  free(menu->instructions);
   free(menu);
 }
